25d100e3ffee71b9773bd2baebd725b08138eb75
Kimberly McCarty <kimberly.mccarty@onepeloton.com>
2025-02-12 13:50:37 -0500

    Made flake multiplatform

--- flake.nix
+++ flake.nix
@@ -3,24 +3,29 @@
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+    systems.url = "github:nix-systems/default";
   };
 
-  outputs = { self, nixpkgs }: let 
-    whisper-model = nixpkgs.legacyPackages.aarch64-darwin.fetchurl {
+  outputs = { self, nixpkgs, systems }: let
+    eachSystem = nixpkgs.lib.genAttrs (import systems);
+    whisper-model = system: nixpkgs.legacyPackages.${system}.fetchurl {
       url = "https://huggingface.co/ggerganov/whisper.cpp/resolve/main/ggml-base.bin";
       sha256 = "sha256-YO1bw90U7qhWST0zQ0m0BXgt3K8AKNS130CINF+6Lv4=";
     };
   in {
-    # Takes a wav file and returns a string
-    packages.aarch64-darwin.whisper_transcribe = nixpkgs.legacyPackages.aarch64-darwin.writeShellApplication {
-    	name = "whisper-transcribe";
-	runtimeInputs = [ nixpkgs.legacyPackages.aarch64-darwin.openai-whisper-cpp whisper-model ];
-	text = ''
-	  whisper-cpp -m ${whisper-model} -f "$1"
-	'';
-    };
+    packages = eachSystem (system: {
+      whisper_transcribe = nixpkgs.legacyPackages.${system}.writeShellApplication {
+        name = "whisper-transcribe";
+        runtimeInputs = [ 
+          nixpkgs.legacyPackages.${system}.openai-whisper-cpp 
+          (whisper-model system)
+        ];
+        text = ''
+          whisper-cpp -m ${whisper-model system} -f "$1"
+        '';
+      };
 
-    packages.aarch64-darwin.default = self.packages.aarch64-darwin.whisper_transcribe;
-
+      default = self.packages.${system}.whisper_transcribe;
+    });
   };
 }
